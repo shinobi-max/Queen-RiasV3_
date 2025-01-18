f540a815fe92789e1ac96bd496159497c152f476
shinobi-max <shinobitech26@gmail.com>
2025-01-17 21:29:12 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,29 +3,29 @@ require("dotenv").config(); // Load environment variables
 module.exports = {
     prefix: process.env.PREFIX || ".", // Command prefix
 
-    ownerName: process.env.OWNER_NAME || "Toxxic-Boy", // Owner name
+    ownerName: process.env.OWNER_NAME || "Anonymous-Boy", // Owner name
 
-    ownerNumber: process.env.OWNER_NUMBER || "2348165846414", // Your WhatsApp number
+    ownerNumber: process.env.OWNER_NUMBER || "50941050126", // Your WhatsApp number
 
-    mode: process.env.MODE || "public", // Bot mode: 'public' or 'private'
+    mode: process.env.MODE || "private", // Bot mode: 'public' or 'private'
 
-    region: process.env.REGION || "Nigeria", // Region
+    region: process.env.REGION || "Haïti", // Region
 
-    botName: process.env.BOT_NAME || "Rias Gremory V3", // Bot name
+    botName: process.env.BOT_NAME || "Anonymous V3", // Bot name
 
-    exifPack: process.env.EXIF_PACK || "RIAS V3 LOVES", // Sticker pack name
+    exifPack: process.env.EXIF_PACK || "SHINOBI V3", // Sticker pack name
 
-    exifAuthor: process.env.EXIF_AUTHOR || "Toxxic", // Author of the sticker pack
+    exifAuthor: process.env.EXIF_AUTHOR || "Anonymous", // Author of the sticker pack
 
     timeZone: process.env.TIME_ZONE || "Africa/Lagos", // Time zone
 
     presenceStatus: process.env.PRESENCE_STATUS || "available", // Bot presence status
 
-    autoRead: process.env.AUTO_READ === "true", // Auto-read messages (true or false)
+    autoRead: process.env.AUTO_READ === "false", // Auto-read messages (true or false)
 
     autoViewStatus: process.env.AUTO_VIEW_STATUS === "true", // Auto-view statuses (true or false)
 
-    autoReact: process.env.AUTO_REACT === "true", // Auto-react (true or false)
+    autoReact: process.env.AUTO_REACT === "false", // Auto-react (true or false)
 
     sessionId: process.env.SESSION_ID || "", // Add Your Session ID here
 
